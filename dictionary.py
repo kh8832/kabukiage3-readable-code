25ceeeff1bcd5042f2e3c11cde27052a23be7f90
kabukiage3 <haruki050648@gmail.com>
2021-09-13 16:40:00 +0900

    仕様3を実装

--- dictionary.py
+++ dictionary.py
@@ -1,6 +1,7 @@
 import argparse
+
 parser = argparse.ArgumentParser(description='このプログラムの説明')
-parser.add_argument('fn', help='file name')
+parser.add_argument('--fn', default=None, help='file name')
 parser.parse_args()
 
 
@@ -10,19 +11,17 @@ def spec1():
 
 # 仕様3
 def spec3(fn):
-    f = open(fn, "r")
+    f = open(fn, "r", encoding="utf-8")
     data = f.readline()
     print(data)
     f.close()
 
 
-
 if __name__ == "__main__":
     args = parser.parse_args()
-    
     fn = args.fn
-    print(fn)
-    if fn is not None:
+    
+    if fn is None:
         spec1()
     else:
         spec3(fn)
