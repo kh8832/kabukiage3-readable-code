02c90ace47243b2fd85025efa84cd34c5103b95c
kabukiage3 <haruki050648@gmail.com>
2021-09-14 13:34:39 +0900

    仕様４を実装

--- dictionary.py
+++ dictionary.py
@@ -5,15 +5,14 @@ parser.add_argument('--fn', default=None, help='file name')
 parser.parse_args()
 
 
-# 仕様1
-def spec1():
-    print("上手")
-
-# 仕様3
-def spec3(fn):
+def readwords(fn):
+    
     f = open(fn, "r", encoding="utf-8")
-    data = f.readline()
-    print(data)
+    wordlist = f.readlines()
+    for word in wordlist:
+        word = word.rstrip('\n')
+        print (word)
+
     f.close()
 
 
@@ -21,7 +20,4 @@ if __name__ == "__main__":
     args = parser.parse_args()
     fn = args.fn
     
-    if fn is None:
-        spec1()
-    else:
-        spec3(fn)
+    readwords(fn)
